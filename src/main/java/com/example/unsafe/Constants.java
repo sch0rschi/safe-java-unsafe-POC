3ec50884b02f9a3b32de53519c865dfc58d065f7
sch0rschi <getschii@gmail.com>
2025-11-27 19:13:51 +0100

    make tests longer

--- src/main/java/com/example/unsafe/Constants.java
+++ src/main/java/com/example/unsafe/Constants.java
@@ -1,7 +1,7 @@
 package com.example.unsafe;
 
 public class Constants {
-    static final long NUMBER_OF_THREADS = 64;
+    static final long NUMBER_OF_THREADS = 128;
     static final long NUMBER_OF_LONGS = 1L;
-    static final long NUMBER_OF_OPERATIONS = 1_000_000L;
+    static final long NUMBER_OF_OPERATIONS = 10_000_000L;
 }
